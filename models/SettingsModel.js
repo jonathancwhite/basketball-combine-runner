e9a2d7a4bab94ff0fccde7431b7e6a8fb62f1047
Jonathan White <contact@jonathancwhite.com>
2024-02-01 07:52:50 -0600

    added discord channel specific settings

--- models/SettingsModel.js
+++ models/SettingsModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const SettingsSchema = new Schema({
+	discord_server_id: { type: String },
+	registered_role_id: { type: String },
 	combine_channel_id: { type: String },
 	player_registration_channel_id: { type: String },
 	coach_registration_channel_id: { type: String },
